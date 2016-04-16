c39e49c353d782634341c85753dcce0df6068717
Sandro Meier <sandro.meier@fidelisfactory.ch>
2016-04-16 14:37:48 -0400

    Prepare DB for storing user ratings.

---
+++ db.py
@@ -0,0 +1,28 @@
+# Creates and manages the ratings DB.
+import sqlite3
+import os
+
+db_file = 'ratings.db'
+
+
+def create_db():
+    """
+    Creates the DB file and the tables. Fails with an error if the DB already exists.
+    """
+    if os.path.isfile(db_file):
+        print("DB already exists!")
+        return
+
+    # Create it!
+    conn = sqlite3.connect(db_file)
+    conn.execute('CREATE TABLE ratings (id INTEGER PRIMARY KEY, tweet_id TEXT, rating INTEGER user TEXT, topics TEXT, created_at TEXT)')
+
+
+def connect():
+    conn = sqlite3.connect(db_file)
+    return conn
+
+
+if __name__ == '__main__':
+    create_db()
+    print('Created DB')
--- server.py
+++ server.py
@@ -1,8 +1,25 @@
+import random
+import hashlib
+import db
+
 from twitter import TwitterAPI
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, abort
+
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'A_REALLY_S3C43T_KEY'
+
+
+def get_user_key():
+    """
+    Get the key of a user.
+    """
+    if "userkey" not in session:
+        # Generate a new user key.
+        userkey = hashlib.md5(str(random.randint(0, 10000)))
+        session['userkey'] = userkey
+
+    return session['userkey']
 
 
 @app.route("/random-tweet")
@@ -11,6 +28,29 @@ def random_tweet():
     tweet_id = random_tweet['id']
     markup = TwitterAPI.get_tweet_markup(tweet_id)
     return render_template('tweet.html', tweet_html=markup)
+
+
+@app.route('/rate-tweet/<tweet_id>')
+def rate_tweet(tweet_id):
+    """
+    Get a rating for the given tweet id.
+    The user is extracted from the session. The rating should be given
+    in a GET parameter called 'rating'.
+    Additionally the articles shown should be in a GET parameter called
+    'articles'
+    """
+
+    # Let's collect all the data
+    rating = request.args.get('rating', None)
+    userkey = get_user_key()
+    articles = request.args.get('articles', None)
+    if rating is None or articles is None:
+        abort(400)
+
+    # TODO: Create an entry in the database.
+
+    # Everything successful.
+    return "", 201
 
 
 if __name__ == '__main__':
