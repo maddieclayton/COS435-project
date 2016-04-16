4fa0e7ea25dc85f9480405ec533e9fcf3482d213
Sandro Meier <sandro.meier@fidelisfactory.ch>
2016-04-16 12:56:21 -0400

    Add method to get random tweet from NY.

--- twitter.py
+++ twitter.py
@@ -1,5 +1,6 @@
 import requests
 import base64
+import random
 
 class TwitterAPI(object):
     """
@@ -16,6 +17,9 @@ class TwitterAPI(object):
 
     # URLs
     _auth_url = _api_url + "/oauth2/token"
+
+    # Application Auth token (Generated using get_app_access_token())
+    _app_auth_token = 'AAAAAAAAAAAAAAAAAAAAABCwugAAAAAAhzrTYmxm0jdo0JtJKGy9nOtAWWE%3DE3Ay7ROEFVtGXEGfrpIeXlRNIVzHTHDdXvRc6W4ZiDdZMTa3tf'
 
     @staticmethod
     def get_app_access_token():
@@ -38,6 +42,30 @@ class TwitterAPI(object):
         response_content = response.json()
         return response_content['access_token']
 
+    @staticmethod
+    def get_tweets():
+        s = requests.Session()
+        request = requests.Request('GET', TwitterAPI._tweets_source_url)
+        request = request.prepare()
+        request = TwitterAPI.add_authorization(request)
+        response = s.send(request)
+        return response.json()
+
+    @staticmethod
+    def get_random_tweet():
+        tweets = TwitterAPI.get_tweets()
+        tweets = tweets['statuses']
+        return tweets[random.randint(0, len(tweets)-1)]
+
+    @staticmethod
+    def add_authorization(request):
+        """
+        :type request: requests.Request
+        """
+        request.headers['Authorization'] = 'Bearer %s' % TwitterAPI._app_auth_token
+        return request
+
 
 if __name__ == '__main__':
-    print("Access Token: %s" % TwitterAPI.get_app_access_token())
+    # print("Access Token: %s" % TwitterAPI.get_app_access_token())
+    print("Tweets: %s" % TwitterAPI.get_random_tweet())
