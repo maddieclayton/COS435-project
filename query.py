ad9d1ed759dcdefc76988b60d4552e756ea5696b
maddieclayton <mec2@princeton.edu>
2016-04-25 22:38:42 -0400

    Added files via upload

--- query.py
+++ query.py
@@ -10,7 +10,13 @@ query = sys.argv[1]
 queryWords = query.split()
 scoreDict = {}
 for word in queryWords:
+<<<<<<< HEAD
+	print (word)
 	if word in keys:
+		print ("s")
+=======
+	if word in keys:
+>>>>>>> 7531d348b35e8b2f5ccf92df593cf9c14eabde76
 		listDocs = csv.reader(open("output/" + word + ".csv"))
 		listDocs1 = next(listDocs)
 		for doc in listDocs1:
@@ -20,4 +26,8 @@ for word in queryWords:
 				scoreDict[doc] = 1/len(listDocs1)
 
 result = max(scoreDict, key=scoreDict.get)
+<<<<<<< HEAD
 print (result)
+=======
+print (result)
+>>>>>>> 7531d348b35e8b2f5ccf92df593cf9c14eabde76
